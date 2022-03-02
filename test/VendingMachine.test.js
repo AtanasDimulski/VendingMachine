a8e0a61ea10afa6bd152a69320016e5b2b34afde
unknown <dimulskiatanas@gmail.com>
2022-03-02 11:14:02 +0100

    Initial commit

---
+++ test/VendingMachine.test.js
@@ -0,0 +1,25 @@
+const VendingMachine = artifacts.require("VendingMachine");
+
+contract("VendingMachine", (accounts) =>{
+    before(async () => {
+        instance =  await VendingMachine.deployed()
+    })
+
+    it("Ensure that the starting balance of the vending machine is 100", async () =>{
+        let balance = await instance.getVendingMachineBalance()
+        assert.equal(balance, 100, 'The initial balance should be 100 donuts')
+    })
+
+    it("Ensures the balance of the vending machine can be updated", async () =>{
+        await instance.restock(100)
+        let balance = await instance.getVendingMachineBalance()
+        assert.equal(balance, 200, 'The balance should be 200 donuts')
+    })
+
+    it("Allows donuts to be purchased", async () =>{
+        await instance.purchase(1, {from: accounts[0], value: web3.utils.toWei('2', 'ether')})
+
+        let balance = await instance.getVendingMachineBalance()
+        assert.equal(balance, 199, 'The balance should be 199 donuts after sale')
+    })
+})
